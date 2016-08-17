aaafa2ad949ebec95096174e48c255fbe16aef2d
André Bernardino Cardoso <andrebernardino@gmail.com>
2016-08-16 22:36:48 -0300

    Add project files.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿//site.js
+(function () {
+
+    var ele = $("#username");
+    ele.text("Shawn Wildermuth");
+
+    var main = $("#main");
+
+    main.on("mouseenter" , function () {
+        main.style.backgroundColor = "#888";
+    });
+    main.on("mouseleave", function () {
+        main.style.backgroundColor = "";
+    });
+
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    });
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+    });
+
+})();
+
+
+//startup();
